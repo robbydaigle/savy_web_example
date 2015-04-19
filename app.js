f1643bbfd56b4cbdbe5ad7ff7e3a8b535111c1cd
Robert Daigle <crunchyturtle@Roberts-MacBook-Pro.local>
2015-04-19 12:46:05 -0400

    Removed unused code

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var users = require('./routes/user');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-// view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,6 +21,8 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 var port = process.env.PORT || 3000;
+
+//This allows node to serve the static polymer elements without having to vulcanize
 app.use('/static', express.static(path.join(process.cwd(), 'bower_components')))
 app.use('/elements', express.static(path.join(process.cwd(), 'app/elements')))
 app.use('/scripts', express.static(path.join(process.cwd(), 'app/scripts')))
@@ -33,6 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -41,16 +43,13 @@ app.use('/users', users);
 app.get('/', function(req,res){
  res.sendfile(__dirname + '/app/index.html');
 });
+
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
-
-// development error handler
-// will print stacktrace
 
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
@@ -74,60 +73,11 @@ app.use(function(err, req, res, next) {
     });
 });
 
-var usernames = {};
-var numUsers = 0;
 io.sockets.on('connection', function (socket) {
   var addedUser = false;
   // when the client emits 'new message', this listens and executes
   socket.on('send message', function (data) {
     console.log(data.message)
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    // remove the username from global usernames list
-    if (addedUser) {
-      delete usernames[socket.username];
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
   });
 });
 
