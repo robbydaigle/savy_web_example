76666089194b32cabe85d6cc236bfbef82be0a7b
Robert Daigle <crunchyturtle@Roberts-MacBook-Pro.local>
2015-04-19 10:50:46 -0400

    I just need the server to recive the message

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var routes = require('./routes/index');
 var users = require('./routes/user');
 
 var app = express();
-
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 // view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +26,7 @@ app.use('/static', express.static(path.join(process.cwd(), 'bower_components')))
 app.use('/elements', express.static(path.join(process.cwd(), 'app/elements')))
 app.use('/scripts', express.static(path.join(process.cwd(), 'app/scripts')))
 app.use('/styles', express.static(path.join(process.cwd(), 'app/styles')))
+
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -46,7 +48,6 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-/// error handlers
 
 // development error handler
 // will print stacktrace
@@ -73,7 +74,64 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.listen(port, function () {
+var usernames = {};
+var numUsers = 0;
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('SendMessage', function (data) {
+    console.log(data)
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
+
+server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
-module.exports = app;
